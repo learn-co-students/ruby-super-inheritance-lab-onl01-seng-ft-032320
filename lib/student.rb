8bd1e7a200f169374d1eccd83029ad9b41c67d84
Christopher <reinkendigital@gmail.com>
2020-04-07 12:56:34 -0500

    Done.

---
+++ lib/student.rb
@@ -0,0 +1,20 @@
+class Student
+
+    #### Attributes ####
+
+    #### Instance Methods ####
+    def initialize
+
+    end
+
+    def hello
+        puts "Hey there! I'm so excited to learn stuff."
+    end
+
+    def raise_hand
+        puts "Pick me!"
+    end
+
+    #### Class Methods####
+
+end
